11527e1dc3ad63066cff7e14baf55f5793c6dd03
Lakshman Kotiki <kotiki.lakshman1996@gmail.com>
2020-02-10 20:09:27 +0530

    added compose endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var lodash = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,35 +13,61 @@ let posts = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', function(req,res) {
-  res.render('home.ejs', { home: homeStartingContent, postHeader: posts.postTitle, postDes: posts.postDescription});
+app.get('/', function(req, res) {
+    res.render('home.ejs', {
+        home: homeStartingContent,
+        posts: posts
+    });
 });
 
-app.get('/about', function(req,res) {
-  res.render('about.ejs', {about: aboutContent});
+app.get('/about', function(req, res) {
+    res.render('about.ejs', { about: aboutContent });
 });
 
-app.get('/contact', function(req,res) {
-  res.render('contact.ejs', {contact: contactContent});
+app.get('/contact', function(req, res) {
+    res.render('contact.ejs', { contact: contactContent });
 });
 
-app.get('/compose', function(req,res) {
-  res.render('compose');
+app.get('/compose', function(req, res) {
+    res.render('compose');
 });
 
-app.post('/compose', function(req,res) {
-  const postData = {
-    postTitle: req.body.postTitle,
-    postDescription: req.body.postDescription
-  };
-  posts.push(postData);
-  res.redirect('/')
+app.post('/compose', function(req, res) {
+    const postData = {
+        postTitle: req.body.postTitle,
+        postDescription: req.body.postDescription
+    };
+    posts.push(postData);
+    res.redirect('/');
 });
 
+app.get('/posts/:postName', function(req, res) {
+    const requestedTitle = req.params.postName;
+    const updatedTitle = lodash._.lowerCase(requestedTitle);
+    posts.forEach(function(item) {
+        const test = lodash._.lowerCase(item.postTitle);
+        if (test === updatedTitle) {
+            console.log("Matched");
+        } else {
+            console.log("Not Matched");
+        }
+    });
+
+});
+
+// app.get('/users/:userId(\d+)', function(req, res) {
+//     console.log(req.params)
+//     res.send('This user route');
+// });
+
+// app.get('/flights/:from-:to', function(req, res) {
+//     console.log(req.params)
+//     res.send("checking request params")
+// })
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    console.log("Server started on port 3000");
 });
