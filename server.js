9f6c63b801fe11e0cb0f1d938079824eded97a36
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-02-11 02:54:31 +0530

    truncated text on home page, added ReadMore text and removed ds_store files

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var lodash = require('lodash');
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+//using some lodash functions in our application
+var _ = require('lodash');
 
 const app = express();
 
+
+//some lorem ipsum content
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hello there. This is Lakshman'\n" + 
+" I'm from Banglore, now doing job as a software engineer in one of the tech startup" + 
+" now am planning to prepare for the DS & Algorithms" + 
+" So that will get job in Big 4 companies"  + 
+" and I like to move to abroad"
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 let posts = [];
 
 app.set('view engine', 'ejs');
@@ -16,19 +22,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//home route
 app.get('/', function(req, res) {
-    res.render('home.ejs', {
+    res.render('home', {
         home: homeStartingContent,
         posts: posts
     });
 });
 
 app.get('/about', function(req, res) {
-    res.render('about.ejs', { about: aboutContent });
+    res.render('about', { about: aboutContent });
 });
 
 app.get('/contact', function(req, res) {
-    res.render('contact.ejs', { contact: contactContent });
+    res.render('contact', { contact: contactContent });
 });
 
 app.get('/compose', function(req, res) {
@@ -44,25 +51,26 @@ app.post('/compose', function(req, res) {
     res.redirect('/');
 });
 
+//posts route using route parameters
 app.get('/posts/:postName', function(req, res) {
     const requestedTitle = req.params.postName;
-    const updatedTitle = lodash._.lowerCase(requestedTitle);
+
+    //converting title to lowercase usgn lodash method to check text is same or not
+    const updatedTitle = _.lowerCase(requestedTitle);
     posts.forEach(function(item) {
-        const test = lodash._.lowerCase(item.postTitle);
-        if (test === updatedTitle) {
-            console.log("Matched");
-        } else {
-            console.log("Not Matched");
+        const testTitle = _.lowerCase(item.postTitle);
+        //checking requested post is existed or not in our server
+        if (testTitle === updatedTitle) {
+            res.render('post', {
+                postName: item.postTitle,
+                postData: item.postDescription
+            });
         }
     });
 
 });
 
-// app.get('/users/:userId(\d+)', function(req, res) {
-//     console.log(req.params)
-//     res.send('This user route');
-// });
-
+//example route to check route parameters
 // app.get('/flights/:from-:to', function(req, res) {
 //     console.log(req.params)
 //     res.send("checking request params")
